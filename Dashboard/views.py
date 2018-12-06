dc39b78df0313dfb654d77435367625e8abdf30e
Owen Miller <millero4@udayton.edu>
2018-12-06 17:31:35 -0500

    trying to make things look a little better

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,4 +1,3 @@
-from django.contrib.sites import requests
 from django.http import HttpResponse
 import csv
 import urllib.request
@@ -9,10 +8,11 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import io
-import numpy as np
 from django.shortcuts import render
 import matplotlib.dates as md
 import datetime as dt
+
+
 
 def home(request):
     return render(request, 'index.html')
@@ -31,45 +31,10 @@ def latest(request):
             data.append((column[1], column[6]))
     except IndexError:
         pass
-    # Remove colume headings from the data
-    data = data[1:]
-
-    # Reversed the data so time flows in positive direction
-    data.reverse()
 
     # Massaging the data for formatting when plotted
-    y = [x[1] for x in data]
-    y = [float(s) for s in y]
-    x = [x[0] for x in data]
+    response = MakeGraph(data)
 
-    x = [dt.datetime.strptime(y, '%Y-%m-%d %H:%M:%S.%f') for y in x]
-    x = md.date2num(x)
-
-
-    fig = plt.figure(figsize=(12, 7.5))
-    ax = fig.add_subplot(111)
-
-    ax.plot_date(x, y, xdate=True)
-    #xfmt = md.DateFormatter('%m-%d %H:%M')
-
-    #ax.xaxis.set_major_formatter(xfmt)
-    ax.set_xlabel('Time')
-    ax.set_ylabel('Temperature [F]')
-
-    # Rotate the ticks on X axis for readability
-    for tick in ax.get_xticklabels():
-        tick.set_rotation(45)
-
-    #start, end = ax.get_xlim()
-
-    # Limit the number of ticks on the x axis readability
-    #ax.set_xticks(np.arange(start, end, 2))
-
-    buf = io.BytesIO()
-    canvas = FigureCanvas(fig)
-    canvas.print_png(buf)
-
-    response = HttpResponse(buf.getvalue(), content_type='image/png')
     return response
 
 
@@ -92,40 +57,41 @@ def daterange(request):
                 data.append((column[1], column[6]))
         except IndexError:
             pass
-        # Remove colume headings from the data
-        data = data[1:]
 
-        # Reversed the data so time flows in positive direction
-        data.reverse()
-
-        # Massaging the data for formatting when plotted
-        y = [x[1] for x in data]
-        y = [float(s) for s in y]
-        x = [x[0] for x in data]
-        x = [dt.datetime.strptime(y, '%Y-%m-%d %H:%M:%S.%f') for y in x]
-        x = md.date2num(x)
-
-        fig = plt.figure(figsize=(12, 7.5))
-        ax = fig.add_subplot(111)
-
-        ax.plot_date(x, y, xdate=True)
-        ax.set_xlabel('Time')
-
-        plt.gcf().subplots_adjust(bottom=0.30)
-        ax.set_ylabel('Temperature [F]')
-
-        # Rotate the ticks on X axis for readability
-        for tick in ax.get_xticklabels():
-            tick.set_rotation(45)
-
-        # start, end = ax.get_xlim()
-
-        # Limit the number of ticks on the x axis readability
-        # ax.set_xticks(np.arange(start, end, 2))
-
-        buf = io.BytesIO()
-        canvas = FigureCanvas(fig)
-        canvas.print_png(buf)
-
-        response = HttpResponse(buf.getvalue(), content_type='image/png')
+        response = MakeGraph(data)
         return response
+
+
+def MakeGraph(data):
+    # Remove colume headings from the data
+    data = data[1:]
+
+    # Reversed the data so time flows in positive direction
+    data.reverse()
+
+    y = [x[1] for x in data]
+    y = [float(s) for s in y]
+    x = [x[0] for x in data]
+
+    x = [dt.datetime.strptime(y, '%Y-%m-%d %H:%M:%S.%f') for y in x]
+    x = md.date2num(x)
+
+    fig = plt.figure(figsize=(12, 7.5))
+    ax = fig.add_subplot(111)
+
+    ax.plot_date(x, y, xdate=True)
+    # xfmt = md.DateFormatter('%m-%d %H:%M')
+
+    # ax.xaxis.set_major_formatter(xfmt)
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Temperature [F]')
+
+    # Rotate the ticks on X axis for readability
+    for tick in ax.get_xticklabels():
+        tick.set_rotation(45)
+
+    buf = io.BytesIO()
+    canvas = FigureCanvas(fig)
+    canvas.print_png(buf)
+
+    return HttpResponse(buf.getvalue(), content_type='image/png')
